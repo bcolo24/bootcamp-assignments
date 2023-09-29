f4416c5d00b4c1ce5ce1340b9aa97fc466b1ff8a
Bethany Colon <bethany.colon28@gmail.com>
2023-09-28 21:37:24 -0400

    9/28 class

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,10 @@
 // Write Your JavaScript Code Here
+var personName = "Beth";
+console.log("My name is " + personName + '.');
+
+var pets = "1";
+console.log("I have " + pets + " pet.");
+
+var funFact = "My keyboard is pink";
+console.log("Fun Fact: " + funFact + ".")
+
