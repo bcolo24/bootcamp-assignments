0d71306ea347e46fd19d254f7166ca37a4805f9b
Bethany Colon <bethany.colon28@gmail.com>
2023-10-24 21:13:44 -0400

    added week 6

---
+++ 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
@@ -0,0 +1,17 @@
+// TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
+var requestUrl = 'https://api.github.com/repos/Twitter/Chill/issues';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Github Repo Issues \n----------');
+    console.log(data);
+    // TODO: Loop through the response
+    for(var i = 0; i < 5; i++){
+      console.log(data[i].url);
+      console.log(data[i].user.login);
+    }
+    // TODO: Console log each issue's URL and each user's login
+  });
